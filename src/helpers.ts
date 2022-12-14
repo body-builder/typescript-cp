7742c02b12e210ed0636f97fff17d98244983bb6
Dan Rumney <dan@sutro.xyz>
2022-12-14 17:16:21 -0600

    style: 💄 use 2-space tabs for indentation

--- src/helpers.ts
+++ src/helpers.ts
@@ -230,8 +230,8 @@ export function get_ignore_list(config: Config, projects: TsProject | TsProject[
 		const safe_projects = !Array.isArray(projects) ? [projects] : projects;
 
 		const ts_exclude_list = safe_projects.map((project) => {
-		  return project.exclude.map((rule) => {
-			// Handle if the exclude pattern contains the name of the root directory
+			return project.exclude.map((rule) => {
+				// Handle if the exclude pattern contains the name of the root directory
 				const rootDirName = project.root_dir.replace(project.base_path + '/', '') + '/';
 				if (rule.startsWith(rootDirName)) {
 					return rule.replace(rootDirName, '');
@@ -411,20 +411,20 @@ async function apply_loaders(raw_content: string, source_path: string, destinati
 
 		processed_content = await used_rules.reduce((content, rule) => {
 			return [...rule.use].reverse().reduce((content, loader) => {
-				let loaderFn;
+					let loaderFn;
 
-				  switch (typeof loader.loader) {
+					switch (typeof loader.loader) {
 						case 'function':
-						  loaderFn = loader.loader;
-						  break;
+							loaderFn = loader.loader;
+							break;
 						case 'string':
-						  loaderFn = require(path.resolve(loader.loader));
-						  break;
-					  default:
+							loaderFn = require(path.resolve(loader.loader));
+							break;
+						default:
 							throw new Error('Invalid loader type');
-				  }
+					}
 
-				  return loaderFn(content, loaderMeta);
+					return loaderFn(content, loaderMeta);
 				},
 				processed_content,
 			);
